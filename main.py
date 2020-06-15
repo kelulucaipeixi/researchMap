008f2535fcb09fb14bb99620b6bb53165e2380fe
kelulucaipeixi <839259294@qq.com>
2020-06-15 13:13:39 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -22,21 +22,21 @@ def getCitedUrl(origin_url):
 
 def crawler(url):
 	global titles
-	headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36'}  
-	request=urllib.request.Request(url,headers=headers)
-	response=urllib.request.urlopen(request)
-	source_code=response.read()
-	soup=BeautifulSoup(source_code,'html.parser')
-	mid_div=soup.find('div',id='gs_res_ccl_mid')
-	h3=mid_div.findAll('h3')
-	for title in h3:
-		res=title.find('a')
-		if res:
-			res=res.find(text=True)
-			titles.append(res)
-		# print(res)
-	bot_div=soup.find('div',id='gs_res_ccl_bot')
 	try:
+		headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36'}  
+		request=urllib.request.Request(url,headers=headers)
+		response=urllib.request.urlopen(request)
+		source_code=response.read()
+		soup=BeautifulSoup(source_code,'html.parser')
+		mid_div=soup.find('div',id='gs_res_ccl_mid')
+		h3=mid_div.findAll('h3')
+		for title in h3:
+			res=title.find('a')
+			if res:
+				res=res.find(text=True)
+				titles.append(res)
+			# print(res)
+		bot_div=soup.find('div',id='gs_res_ccl_bot')	
 		next_page=bot_div.find('td',align='left')
 		link=next_page.find('a')['href']
 		link="https://scholar.google.com.hk"+link
